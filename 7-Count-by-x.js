e9f773d62d64491f4c5e1634f6e4f23b91801f98
suyanto98 <suyantolearn09@gmail.com>
2024-02-12 16:14:52 +0700

    day-7 Count by x

---
+++ 7-Count-by-x.js
@@ -0,0 +1,21 @@
+// Code-challange
+// 7 of 366 day
+
+
+// function countBy(x, n) {
+//     let z = [];
+//     for(let i = 1; i <= n; i++){
+//         z.push(x * i)
+//     }
+//     return z
+// }
+
+// function countBy(x, n) {
+//     let newArray = [...new Array(n)].map((el, i) => (i + 1) * x)
+//     return newArray
+// }
+
+const countBy = (x, n) => [... new Array(n)].map((el, i) => (i+1) * x)
+
+
+console.log(countBy(2,5))
