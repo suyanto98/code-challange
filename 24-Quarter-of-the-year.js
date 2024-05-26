19611449bcd04fbb6edc9d7ef5189d7cef19151e
suyanto98 <suyantolearn09@gmail.com>
2024-05-26 21:24:50 +0700

    day/24 Quarter of the year

---
+++ 24-Quarter-of-the-year.js
@@ -0,0 +1,13 @@
+// Code-challange
+// 23 of 366 day
+
+// const quarterOf = (month) => {
+//     if(month <= 3) return 1;
+//     if(month <= 6) return 2;
+//     if(month <= 9) return 3;
+//     return 4;
+// }
+
+const quarterOf = (month) => month <= 3 ? 1 : month <= 6 ? 2 : month <= 9 ? 3 : 4 
+
+console.log(quarterOf(3), 1);
