58aefd960b73f4033515a248270266bb906103ae
suyanto98 <suyantolearn09@gmail.com>
2024-06-11 06:16:57 +0700

    Do I get a bonus?

---
+++ 28-o I get a bonus.js
@@ -0,0 +1,12 @@
+// Code-challange
+// 27 of 366 day
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96/solutions/javascript
+
+// function bonusTime(salary, bonus) {
+//     if(bonus) return `£${salary * 10}`
+//     return `£${salary}`
+// }
+
+const bonusTime = (salary, bonus) => bonus ? `£${salary * 10}` : `£${salary}` 
+
+console.log(bonusTime(10000, true)) //'£100000'
