cb044118a7f874d883d60b7ea3b6f4cc111a4058
suyanto98 <suyantolearn09@gmail.com>
2024-07-19 07:01:01 +0700

    day43/ Convert a String to a Number!

---
+++ 43-Convert a String to a Number!.js
@@ -0,0 +1,11 @@
+// const stringToNumber = function(str){
+//    return Number(str)
+// }
+
+// const stringToNumber = function(str){
+//     return parseInt(str)
+// }
+
+const stringToNumber = (str) => +str
+
+console.log(stringToNumber("1234"));
