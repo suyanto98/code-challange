96b22f17a6bcd74bb8f1fa6e729d354e746d1dc9
suyanto98 <suyantolearn09@gmail.com>
2024-07-24 21:33:09 +0700

    day46/ Keep Hydrated!

---
+++ 46-Keep Hydrated!.js
@@ -0,0 +1,10 @@
+// Code-challange
+// 46 of 366 day
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/solutions/javascript
+
+// const litres = (time) => Math.floor(time * 0.5);
+
+const litres = (time) => parseInt(time / 2);
+
+
+console.log(litres(2));
