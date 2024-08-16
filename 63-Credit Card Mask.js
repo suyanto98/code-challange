1453d9c7f139f637e3f04bbc884a59025b784c12
suyanto98 <suyantolearn09@gmail.com>
2024-08-16 15:02:58 +0700

    days63/Credit Card Mask

---
+++ 63-Credit Card Mask.js
@@ -0,0 +1,24 @@
+// Code-challange
+// 63 of 366 day
+// 
+
+// function maskify(cc) {
+//     let result = ''
+//     for (let i = 0; i < cc.length ; i++) {
+//         if(i < cc.length - 4){
+//             result += '#'
+//         } else {
+//             result += cc[i]
+//         }
+//     }
+
+//     return result
+// }
+
+// function maskify(cc){
+//     return [... cc].map((value, index) => index < cc.length - 4 ? "#" : value).join('')
+// }
+
+const maskify = (cc) => [... cc].map((value, index) => index < cc.length - 4 ? "#" : value).join('')
+
+console.log(maskify('4556364607935616')); //#1111
