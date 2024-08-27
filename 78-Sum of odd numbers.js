65d50db4f3561975d65216a13e2d0e9c6e76a2dd
suyanto98 <suyantolearn09@gmail.com>
2024-08-27 12:48:51 +0700

    days78/Sum of odd numbers

---
+++ 78-Sum of odd numbers.js
@@ -0,0 +1,7 @@
+// Code-challange
+// 78 of 366 day
+// https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/solutions/javascript
+
+const rowSumOddNumbers = (n) => n * n * n 
+
+console.log(rowSumOddNumbers(2));
